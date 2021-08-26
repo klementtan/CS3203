c87909424ede737c363869d46b7a1115963d40d7
zhiayang <gpg@tsumaran.ai>
2021-08-27 02:04:18 +0800

    tests: add preliminary tests for lexer

---
+++ Team34/Code34/src/unit_testing/src/test_lexer.cpp
@@ -0,0 +1,85 @@
+// test_lexer.cpp
+
+#define CATCH_CONFIG_FAST_COMPILE 1
+#include "catch.hpp"
+
+#include <zst.h>
+#include <zpr.h>
+#include "simple_parser.h"
+
+static void require(bool b)
+{
+    REQUIRE(b);
+}
+
+using namespace simple_parser;
+
+template <typename... Tokens>
+static void check_tokens(zst::str_view sv, Tokens&&... tokens)
+{
+    auto check_token = [&sv](auto&& tok) {
+        require(getNextToken(sv) == tok);
+    };
+
+    (check_token(static_cast<Tokens&&>(tokens)), ...);
+    require(getNextToken(sv) == TT::EndOfFile);
+}
+
+// first, the single-character tokens
+TEST_CASE("basic tokens")
+{
+    check_tokens("", TT::EndOfFile);
+    check_tokens("<", TT::LAngle);
+    check_tokens(">", TT::RAngle);
+    check_tokens("{", TT::LBrace);
+    check_tokens("}", TT::RBrace);
+    check_tokens("(", TT::LParen);
+    check_tokens(")", TT::RParen);
+    check_tokens("!", TT::Exclamation);
+    check_tokens("&&", TT::LogicalAnd);
+    check_tokens("||", TT::LogicalOr);
+    check_tokens("=", TT::Equal);
+    check_tokens("!=", TT::NotEqual);
+    check_tokens("==", TT::EqualsTo);
+    check_tokens(">=", TT::GreaterEqual);
+    check_tokens("<=", TT::LessEqual);
+    check_tokens("+", TT::Plus);
+    check_tokens("-", TT::Minus);
+    check_tokens("*", TT::Asterisk);
+    check_tokens("/", TT::Slash);
+    check_tokens("%", TT::Percent);
+    check_tokens(";", TT::Semicolon);
+}
+
+// then, some identifiers. simple grammar has no underscores.
+TEST_CASE("basic identifiers")
+{
+    check_tokens("a12345bcdef", TT::Identifier);
+    check_tokens("abcdef", TT::Identifier);
+    check_tokens("a12345", TT::Identifier);
+    check_tokens("a", TT::Identifier);
+}
+
+// then, numbers. these are only integers, and they are all positive
+TEST_CASE("basic numbers")
+{
+    check_tokens("0", TT::Number);
+    check_tokens("12345", TT::Number);
+    check_tokens("9999999999", TT::Number);
+}
+
+// now, check multiple tokens.
+TEST_CASE("basic tokens 2")
+{
+    check_tokens("= =", TT::Equal, TT::Equal);
+    check_tokens("< =", TT::LAngle, TT::Equal);
+    check_tokens("> =", TT::RAngle, TT::Equal);
+    check_tokens("! =", TT::Exclamation, TT::Equal);
+}
+
+TEST_CASE("token sequencing")
+{
+    check_tokens("000aaa000bbb", TT::Number, TT::Identifier);
+    check_tokens("foobar(123 456 abcdef);", TT::Identifier, TT::LParen, TT::Number, TT::Number, TT::Identifier,
+        TT::RParen, TT::Semicolon);
+}
