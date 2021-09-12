91d1ca7e50e875bc1a4b745eb2173eb5105557b6
zhiayang <zhiayang@nascentnoodle.com>
2021-09-13 00:41:09 +0800

    fix formatting

--- Team34/Code34/src/spa/src/pql/eval/pattern.cpp
+++ Team34/Code34/src/spa/src/pql/eval/pattern.cpp
@@ -33,10 +33,11 @@ namespace pql::ast
         assert(this->assignment_declaration->design_ent == DESIGN_ENT::ASSIGN);
 
         auto domain = tbl->getDomain(this->assignment_declaration);
-        for(auto it = domain.begin(); it != domain.end(); )
+        for(auto it = domain.begin(); it != domain.end();)
         {
             bool should_erase = false;
-            auto assign_stmt = dynamic_cast<s_ast::AssignStmt*>(pkb->uses_modifies.statements[it->getStmtNum() - 1]->stmt);
+            auto assign_stmt =
+                dynamic_cast<s_ast::AssignStmt*>(pkb->uses_modifies.statements[it->getStmtNum() - 1]->stmt);
             assert(assign_stmt);
 
             // check the rhs first, since it requires less table operations
