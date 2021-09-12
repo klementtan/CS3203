fe362ffd037ba706f3a832595ffbbe92512242a7
zhiayang <zhiayang@nascentnoodle.com>
2021-09-12 12:58:43 +0800

    implement UsesP

--- Team34/Code34/src/spa/src/pql/eval/evaluator.cpp
+++ Team34/Code34/src/spa/src/pql/eval/evaluator.cpp
@@ -475,45 +475,52 @@ namespace pql::eval
         using PqlException = pql::exception::PqlException;
 
         assert(rel);
-        auto user_name = dynamic_cast<ast::EntName*>(rel->user);
-        auto user_decl = dynamic_cast<ast::DeclaredEnt*>(rel->user);
-        auto user_all  = dynamic_cast<ast::AllEnt*>(rel->user);
-        auto ent_name = dynamic_cast<ast::EntName*>(rel->ent);
-        auto ent_decl = dynamic_cast<ast::DeclaredEnt*>(rel->ent);
-        auto ent_all  = dynamic_cast<ast::AllEnt*>(rel->ent);
+        auto proc_name = dynamic_cast<ast::EntName*>(rel->user);
+        auto proc_decl = dynamic_cast<ast::DeclaredEnt*>(rel->user);
+        auto proc_all = dynamic_cast<ast::AllEnt*>(rel->user);
+        auto var_name = dynamic_cast<ast::EntName*>(rel->ent);
+        auto var_decl = dynamic_cast<ast::DeclaredEnt*>(rel->ent);
+        auto var_all = dynamic_cast<ast::AllEnt*>(rel->ent);
 
         // this should not happen, since Uses(_, foo) is invalid according to the specs
-        if(user_all)
+        if(proc_all)
             throw PqlException("pql::eval", "first argument of Uses cannot be '_'");
 
-        if(user_name && ent_name)
+        if(proc_decl && proc_decl->declaration->design_ent != ast::DESIGN_ENT::PROCEDURE)
+            throw PqlException("pql::eval", "entity for first argument of Uses must be a procedure");
+
+        if(var_decl && var_decl->declaration->design_ent != ast::DESIGN_ENT::VARIABLE)
+            throw PqlException("pql::eval", "entity for second argument of Uses must be a variable");
+
+
+        if(proc_name && var_name)
         {
             util::log("pql::eval", "Processing UsesP(EntName, EntName)");
-            if(!m_pkb->uses_modifies.isUses(user_name->name, ent_name->name))
-                throw PqlException("pql::eval", "{} is always false", rel->toString(), ent_name->name);
+            if(!m_pkb->uses_modifies.isUses(proc_name->name, var_name->name))
+                throw PqlException("pql::eval", "{} is always false", rel->toString(), var_name->name);
 
             return;
         }
-        else if(user_name && ent_decl)
+        else if(proc_name && var_decl)
         {
             util::log("pql::eval", "Processing UsesP(EntName, DeclaredStmt)");
-            auto used_vars = m_pkb->uses_modifies.getUsesVars(user_name->name);
+            auto used_vars = m_pkb->uses_modifies.getUsesVars(proc_name->name);
             if(used_vars.empty())
                 throw PqlException("pql::eval", "{} is always false; {} doesn't use any variables", rel->toString());
 
             std::unordered_set<table::Entry> new_domain { };
-            auto old_domain = m_table->getDomain(ent_decl->declaration);
 
             for(const auto& var : used_vars)
-                new_domain.insert(table::Entry(ent_decl->declaration, var));
+                new_domain.insert(table::Entry(var_decl->declaration, var));
 
-            m_table->upsertDomains(ent_decl->declaration, table::entry_set_intersect(old_domain, new_domain));
+            auto old_domain = m_table->getDomain(var_decl->declaration);
+            m_table->upsertDomains(var_decl->declaration, table::entry_set_intersect(old_domain, new_domain));
             return;
         }
-        else if(user_name && ent_all)
+        else if(proc_name && var_all)
         {
             util::log("pql::eval", "Processing UsesP(EntName, _)");
-            auto used_vars = m_pkb->uses_modifies.getUsesVars(user_name->name);
+            auto used_vars = m_pkb->uses_modifies.getUsesVars(proc_name->name);
             if(used_vars.empty())
                 throw PqlException("pql::eval", "{} is always false; {} doesn't use any variables", rel->toString());
 
@@ -521,14 +528,59 @@ namespace pql::eval
             return;
         }
 
-        else if(user_decl && ent_name)
+        else if(proc_decl && var_name)
         {
+            util::log("pql::eval", "Processing UsesP(DeclaredStmt, EntName)");
+
+            auto procs_using = m_pkb->uses_modifies.getUses(ast::DESIGN_ENT::PROCEDURE, var_name->name);
+            if(procs_using.empty())
+                throw PqlException(
+                    "pql::eval", "{} is always false; {} no procedure uses '{}'", rel->toString(), var_name->name);
+
+            std::unordered_set<table::Entry> new_domain {};
+            for(const auto& proc_name : procs_using)
+                new_domain.insert(table::Entry(proc_decl->declaration, proc_name));
+
+            auto old_domain = m_table->getDomain(var_decl->declaration);
+            m_table->upsertDomains(proc_decl->declaration, table::entry_set_intersect(old_domain, new_domain));
+            return;
         }
-        else if(user_decl && ent_decl)
+        else if(proc_decl && var_decl)
         {
+            util::log("pql::eval", "Processing UsesP(DeclaredStmt, DeclaredStmt)");
+            for(const auto& entry : m_table->getDomain(proc_decl->declaration))
+            {
+                auto proc_used_vars = m_pkb->uses_modifies.getUsesVars(entry.getVal());
+                if(proc_used_vars.empty())
+                    continue;
+
+                auto proc_entry = table::Entry(proc_decl->declaration, entry.getVal());
+                for(const auto& var_name : proc_used_vars)
+                {
+                    auto var_entry = table::Entry(var_decl->declaration, var_name);
+                    util::log("pql::eval", "{} adds Join({}, {}),", rel->toString(), proc_entry.toString(),
+                        var_entry.toString());
+
+                    m_table->addJoin(proc_entry, var_entry);
+                }
+            }
         }
-        else if(user_decl && ent_all)
+        else if(proc_decl && var_all)
         {
+            util::log("pql::eval", "Processing UsesP(DeclaredStmt, _)");
+            std::unordered_set<table::Entry> new_domain {};
+
+            for(const auto& entry : m_table->getDomain(proc_decl->declaration))
+            {
+                auto proc_used_vars = m_pkb->uses_modifies.getUsesVars(entry.getVal());
+                if(proc_used_vars.empty())
+                    continue;
+
+                new_domain.insert(entry);
+            }
+
+            m_table->upsertDomains(proc_decl->declaration,
+                table::entry_set_intersect(new_domain, m_table->getDomain(proc_decl->declaration)));
         }
 
         throw PqlException("pql::eval", "unreachable");
