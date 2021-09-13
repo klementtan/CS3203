a9bc56720e5580cbf8f8ae027e779dbce20ec9c1
zhiayang <zhiayang@nascentnoodle.com>
2021-09-13 14:50:21 +0800

    attempt 2

--- Team34/Tests34/sys_tests_1/run.bat
+++ Team34/Tests34/sys_tests_1/run.bat
@@ -1,16 +1,14 @@
-
 @echo off
 title system test 1 queries
 
-..\..\Code34\build_win\x86-Debug\src\autotester\autotester.exe Source.simple Follows.txt outputFollows.xml
-..\..\Code34\build_win\x86-Debug\src\autotester\autotester.exe Source.simple FollowsT.txt outputFollowsT.xml
-..\..\Code34\build_win\x86-Debug\src\autotester\autotester.exe Source.simple Parent.txt outputParent.xml
-..\..\Code34\build_win\x86-Debug\src\autotester\autotester.exe Source.simple ParentT.txt outputParentT.xml
-..\..\Code34\build_win\x86-Debug\src\autotester\autotester.exe Source.simple Uses.txt outputUses.xml
-..\..\Code34\build_win\x86-Debug\src\autotester\autotester.exe Source.simple Modifies.txt outputModifies.xml
-..\..\Code34\build_win\x86-Debug\src\autotester\autotester.exe Source.simple NoClause.txt outputNoClause.xml
-..\..\Code34\build_win\x86-Debug\src\autotester\autotester.exe Source.simple Pattern.txt outputPattern.xml
+..\..\Code34\build\x86-Release\src\autotester\autotester.exe Source.simple Follows.txt output-Follows.xml
+..\..\Code34\build\x86-Release\src\autotester\autotester.exe Source.simple FollowsT.txt output-FollowsT.xml
+..\..\Code34\build\x86-Release\src\autotester\autotester.exe Source.simple Parent.txt output-Parent.xml
+..\..\Code34\build\x86-Release\src\autotester\autotester.exe Source.simple ParentT.txt output-ParentT.xml
+..\..\Code34\build\x86-Release\src\autotester\autotester.exe Source.simple Uses.txt output-Uses.xml
+..\..\Code34\build\x86-Release\src\autotester\autotester.exe Source.simple Modifies.txt output-Modifies.xml
+..\..\Code34\build\x86-Release\src\autotester\autotester.exe Source.simple NoClause.txt output-NoClause.xml
+..\..\Code34\build\x86-Release\src\autotester\autotester.exe Source.simple Pattern.txt output-Pattern.xml
 
 echo FINISHED
-pause
 exit
