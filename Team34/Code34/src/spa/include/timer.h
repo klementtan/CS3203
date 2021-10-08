58b91ce5b9a3ce6d16a1086c1056c125e1d4f593
geoboom <leegeorgie52@gmail.com>
2021-10-08 23:56:40 +0800

    Add timer stuff

---
+++ Team34/Code34/src/spa/include/timer.h
@@ -0,0 +1,34 @@
+#include <bits/stdc++.h>
+#include <iostream>
+#include <chrono>
+
+namespace bench
+{
+  struct Timer {
+    std::string file;
+    int line;
+    std::string fn, title;
+    std::chrono::time_point<std::chrono::steady_clock> start;
+    Timer(std::string file, int line, std::string fn, std::string title)
+      : file(file),
+        line(line),
+        fn(fn),
+        title(title),
+        start(std::chrono::steady_clock::now())
+    {}
+    ~Timer() {
+      const auto elapsed = std::chrono::duration_cast<std::chrono::microseconds>(std::chrono::steady_clock::now() - start).count();
+      std::string where = file + std::string(":") + std::to_string(line);
+      std::string text = "[TIMER: " + title + "];"
+                          + "function=" + fn + ";"
+                          + "where=" + where + ";"
+                          + "elapsed=" + std::to_string(elapsed) + "ms";
+      std::cout << text << "\n";
+    }
+  };
+}
+
+#define START_BENCH_TIMER(title) \
+        bench::Timer timer(__FILE__, __LINE__, __FUNCTION__, title)
+
+
