7a5cc7bfe5cd7bf9716685b30c8f5100866eed77
geoboom <leegeorgie52@gmail.com>
2021-10-10 14:20:44 +0800

    Incorporate changes based on PR comments

--- Team34/Code34/src/spa/include/timer.h
+++ Team34/Code34/src/spa/include/timer.h
@@ -1,34 +1,36 @@
-#include <iostream>
 #include <chrono>
 #include <string>
+#include <zpr.h>
 
 namespace bench
 {
-  struct Timer {
-    std::string file;
-    int line;
-    std::string fn, title;
-    std::chrono::time_point<std::chrono::steady_clock> start;
-    Timer(std::string file, int line, std::string fn, std::string title)
-      : file(file),
-        line(line),
-        fn(fn),
-        title(title),
-        start(std::chrono::steady_clock::now())
-    {}
-    ~Timer() {
-      const auto elapsed = std::chrono::duration_cast<std::chrono::microseconds>(std::chrono::steady_clock::now() - start).count();
-      std::string where = file + std::string(":") + std::to_string(line);
-      std::string text = "[TIMER: " + title + "];"
-                          + "function=" + fn + ";"
-                          + "where=" + where + ";"
-                          + "elapsed=" + std::to_string(elapsed) + "ms";
-      std::cout << text << "\n";
-    }
-  };
+    struct Timer
+    {
+        std::string file;
+        int line;
+        std::string fn, title;
+        std::chrono::time_point<std::chrono::steady_clock> start;
+        Timer(std::string file, int line, std::string fn, std::string title)
+            : file(std::move(file)), line(std::move(line)), fn(std::move(fn)), title(std::move(title)),
+              start(std::chrono::steady_clock::now())
+        {
+        }
+        ~Timer()
+        {
+            const auto elapsed =
+                std::chrono::duration_cast<std::chrono::microseconds>(std::chrono::steady_clock::now() - start).count();
+            std::string where = file + std::string(":") + std::to_string(line);
+            std::string text = "[TIMER: " + title + "];" + "function=" + fn + ";" + "where=" + where + ";" +
+                               "elapsed=" + std::to_string(elapsed) + "ms";
+            zpr::println(text);
+        }
+    };
 }
 
-#define START_BENCH_TIMER(title) \
-        bench::Timer timer(__FILE__, __LINE__, __FUNCTION__, title)
-
+#ifndef ENABLE_BENCHMARK
+static constexpr inline void dummy_fn() { }
+#define START_BENCHMARK_TIMER(...) dummy_fn()
+#else
+#define START_BENCHMARK_TIMER(title) bench::Timer timer(__FILE__, __LINE__, __FUNCTION__, title)
+#endif
 
