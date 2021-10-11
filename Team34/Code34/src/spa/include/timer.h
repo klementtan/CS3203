b9ac681986a84f414a898f3638d7b31f1769f297
klementtan <klementtan@gmail.com>
2021-10-12 00:25:43 +0800

    bench: Remove full path from file name

--- Team34/Code34/src/spa/include/timer.h
+++ Team34/Code34/src/spa/include/timer.h
@@ -2,25 +2,24 @@
 #include <string>
 #include <zpr.h>
 #include <util.h>
+#include <string.h>
+
 
 namespace bench
 {
     struct Timer
     {
-        std::string file;
-        int line;
         std::string fn, title;
         std::chrono::time_point<std::chrono::steady_clock> start;
-        Timer(std::string file, int line, std::string fn, std::string title)
-            : file(std::move(file)), line(std::move(line)), fn(std::move(fn)), title(std::move(title)),
-              start(std::chrono::steady_clock::now())
+        Timer(std::string fn, std::string title)
+            : fn(std::move(fn)), title(std::move(title)), start(std::chrono::steady_clock::now())
         {
         }
         ~Timer()
         {
             const auto elapsed =
                 std::chrono::duration_cast<std::chrono::microseconds>(std::chrono::steady_clock::now() - start).count();
-            util::logfmt("TIMER", "{}: function={}; where={}:{}; elapsed={}ms", title, fn, file, line, elapsed);
+            util::logfmt("TIMER", "{}: function={}; elapsed={}ms", title, fn, elapsed);
         }
     };
 }
@@ -29,5 +28,6 @@ namespace bench
 static constexpr inline void dummy_fn() { }
 #define START_BENCHMARK_TIMER(...) dummy_fn()
 #else
-#define START_BENCHMARK_TIMER(title) bench::Timer timer(__FILE__, __LINE__, __FUNCTION__, title)
+
+#define START_BENCHMARK_TIMER(title) bench::Timer timer(__FUNCTION__, title)
 #endif
